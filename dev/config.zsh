b8bba47c6db38d4122d6a62b1915cdd749165649
Arik Galansky <arik.galansky@gmail.com>
2017-11-02 22:26:28 +0200

    fixes

--- dev/config.zsh
+++ dev/config.zsh
@@ -1,1 +1,10 @@
-config.zsh
+# Set one temp dir preventing OS to recreate it again and again
+export TEMPDIR=/tmp
+ 
+# Set lang for UTF8 especially for database
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+ 
+# virtualenv
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
